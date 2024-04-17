9a25bbe4467b043656722c8250f2063c0006fefa
Evin Oxley <91544713+EvinIkuyama@users.noreply.github.com>
2024-04-17 17:57:54 +0100

    Game Instance + Sensitivity Saving

--- Main Project/NineLivesProject/Source/NineLivesProject/GameInstanceSave.cpp
+++ Main Project/NineLivesProject/Source/NineLivesProject/GameInstanceSave.cpp
@@ -1,9 +1,9 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "LevelSpeedSave.h"
+#include "GameInstanceSave.h"
 
-void ULevelSpeedSave::CallLevelData(int levelNumber, int& levelRetries, int& levelSeconds, int& levelStars)
+void UGameInstanceSave::CallLevelData(int levelNumber, int& levelRetries, int& levelSeconds, int& levelStars)
 {
 	levelRetries = levelSaveRetries[levelNumber];
 	levelSeconds = levelSaveSeconds[levelNumber];
