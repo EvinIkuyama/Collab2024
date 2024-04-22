5542c6ab14d95fa8840e23b4eb84e848bb123232
Tom Lawrence <114944480+Tomax1z@users.noreply.github.com>
2024-04-22 12:12:59 +0100

    Made ammo count and visual aid on HUD

--- Main Project/NineLivesProject/Source/NineLivesProject/Public/Actors/Potion.h
+++ Main Project/NineLivesProject/Source/NineLivesProject/Public/Actors/Potion.h
@@ -35,7 +35,7 @@ public:
 	UPROPERTY(BlueprintReadWrite,EditAnywhere)
 	Enum_ActiveElement Type;
 
-	UFUNCTION()
+	UFUNCTION(BlueprintCallable)
 	virtual void PickedUp_Implementation(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool
 	                                     bFromSweep, const FHitResult& SweepResult);
 protected:
