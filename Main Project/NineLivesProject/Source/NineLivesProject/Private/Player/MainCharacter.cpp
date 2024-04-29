4b119a94b77bee4a7370d51df49009e3754b3435
Tom Lawrence <114944480+Tomax1z@users.noreply.github.com>
2024-04-29 19:41:42 +0100

    Transfered blueprint to code. Try and fix floaty wall run

--- Main Project/NineLivesProject/Source/NineLivesProject/Private/Player/MainCharacter.cpp
+++ Main Project/NineLivesProject/Source/NineLivesProject/Private/Player/MainCharacter.cpp
@@ -3,6 +3,9 @@
 
 #include "Player/MainCharacter.h"
 
+#include "Components/CapsuleComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
 
 // Sets default values
 AMainCharacter::AMainCharacter()
@@ -10,13 +13,16 @@ AMainCharacter::AMainCharacter()
 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
-	playerCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
-	playerCamera -> SetupAttachment(GetRootComponent());
-	playerCamera -> SetRelativeLocation(FVector(-10.0f, 0.0f, 60.0f));
+	_PlayerCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	_PlayerCamera -> SetupAttachment(GetRootComponent());
+	_PlayerCamera -> SetRelativeLocation(FVector(-10.0f, 0.0f, 60.0f));
+
+	_PlayerMovement = GetCharacterMovement();
+	_PlayerVelocity = _PlayerMovement->GetLastUpdateVelocity();
 	
-	CameraForwardVector = playerCamera->GetForwardVector();
+	CameraForwardVector = _PlayerCamera->GetForwardVector();
 	WallJumpingVector = CameraForwardVector * wallJumpFactor;
-	launchPower = wallJumpFactor * 1.2f;
+	
 }
 
 // Called when the game starts or when spawned
@@ -30,8 +36,70 @@ void AMainCharacter::JumpOffWall(bool isWallRunning)
 {
 	if(isWallRunning)
 	{
-		LaunchCharacter(FVector (WallJumpingVector.X, WallJumpingVector.Y, launchPower), true, true);
+		LaunchCharacter(FVector (_PlayerVelocity.X * wallJumpFactor, _PlayerVelocity.Y * wallJumpFactor, wallJumpFactor), true, true);
 	}
+}
+
+void AMainCharacter::WallRunning(float alpha)
+{
+	_PlayerVelocity.Set(_PlayerVelocity.X, _PlayerVelocity.Y, 0.0f);
+	_PlayerMovement->GravityScale = 0.0f;
+}
+
+void AMainCharacter::StopWallRunning(bool isWallRunning)
+{
+	_PlayerMovement->GravityScale = 1.75f;
+	if(coyoteTime < 1.0f)
+	{
+		isWallRunning = true;
+		GetWorld()->GetTimerManager().SetTimer(_CoyoteTimer, this, &AMainCharacter::IncreaseTimerValue, 0.1f, true);
+	}
+	else if(coyoteTime > 1.0f)
+	{
+		isWallRunning = false;
+		StopTimerValue();
+	}
+}
+
+void AMainCharacter::SlidingEvent()
+{
+	if(_PlayerMovement->IsMovingOnGround())
+	{
+		GetCapsuleComponent()->SetCapsuleHalfHeight(20.0f, true);
+	}
+}
+
+void AMainCharacter::SlidingPhysics()
+{
+	FVector SlideForwardVector(GetActorForwardVector().X, GetActorForwardVector().Y, 0.0f);
+	SlideMultiplier = SlideForwardVector * slideFactor;
+	_PlayerMovement->AddImpulse(SlideMultiplier);
+}
+
+bool AMainCharacter::EndingSlide()
+{
+	if(_PlayerMovement->IsMovingOnGround())
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+void AMainCharacter::IncreaseTimerValue()
+{
+	if(_CoyoteTimer.IsValid())
+	{
+		coyoteTime += 0.1f;
+	}
+}
+
+void AMainCharacter::StopTimerValue()
+{
+	GetWorld()->GetTimerManager().ClearTimer(_CoyoteTimer);
+	coyoteTime = 0.0f;
 }
 
 // Called every frame
--- Main Project/NineLivesProject/Source/NineLivesProject/Public/Player/MainCharacter.h
+++ Main Project/NineLivesProject/Source/NineLivesProject/Public/Player/MainCharacter.h
@@ -21,16 +21,43 @@ protected:
 	virtual void BeginPlay() override;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-	TObjectPtr<UCameraComponent> playerCamera;
+	TObjectPtr<UCameraComponent> _PlayerCamera;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TObjectPtr<UCharacterMovementComponent> _PlayerMovement;
+	UPROPERTY()
+	FVector _PlayerVelocity;
 
+	//Wall Running
 	UFUNCTION(BlueprintCallable)
 	void JumpOffWall(bool isWallRunning);
+	UFUNCTION(BlueprintCallable)
+	void WallRunning(float alpha);
+	UFUNCTION(BlueprintCallable)
+	void StopWallRunning(bool isWallRunning);
+
+	//Sliding
+	UFUNCTION(BlueprintCallable)
+	void SlidingEvent();
+	UFUNCTION(BlueprintCallable)
+	void SlidingPhysics();
+	UFUNCTION(BlueprintCallable)
+	bool EndingSlide();
+	
+	void IncreaseTimerValue();
+	void StopTimerValue();
 	
 	float wallJumpFactor = 750.0f;
 	float launchPower;
+	float slideFactor = 17000.0f;
 	
 	FVector WallJumpingVector;
 	FVector CameraForwardVector;
+	FVector SlideMultiplier;
+	
+	FTimerHandle _CoyoteTimer;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float coyoteTime;
 
 public:
 	// Called every frame
