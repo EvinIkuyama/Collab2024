7e4c89ad7a7f5293f4419f0372fb4a11bbba197a
Tom Lawrence <114944480+Tomax1z@users.noreply.github.com>
2024-04-29 13:03:42 +0100

    Fix the gravity scale

--- Main Project/NineLivesProject/Source/NineLivesProject/Private/Player/MainCharacter.cpp
+++ Main Project/NineLivesProject/Source/NineLivesProject/Private/Player/MainCharacter.cpp
@@ -9,6 +9,14 @@ AMainCharacter::AMainCharacter()
 {
 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+
+	playerCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	playerCamera -> SetupAttachment(GetRootComponent());
+	playerCamera -> SetRelativeLocation(FVector(-10.0f, 0.0f, 60.0f));
+	
+	CameraForwardVector = playerCamera->GetForwardVector();
+	WallJumpingVector = CameraForwardVector * wallJumpFactor;
+	launchPower = wallJumpFactor * 1.2f;
 }
 
 // Called when the game starts or when spawned
@@ -16,6 +24,14 @@ void AMainCharacter::BeginPlay()
 {
 	Super::BeginPlay();
 	
+}
+
+void AMainCharacter::JumpOffWall(bool isWallRunning)
+{
+	if(isWallRunning)
+	{
+		LaunchCharacter(FVector (WallJumpingVector.X, WallJumpingVector.Y, launchPower), true, true);
+	}
 }
 
 // Called every frame
--- Main Project/NineLivesProject/Source/NineLivesProject/Public/Player/MainCharacter.h
+++ Main Project/NineLivesProject/Source/NineLivesProject/Public/Player/MainCharacter.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Camera/CameraComponent.h"
 #include "GameFramework/Character.h"
 #include "MainCharacter.generated.h"
 
@@ -18,6 +19,18 @@ public:
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TObjectPtr<UCameraComponent> playerCamera;
+
+	UFUNCTION(BlueprintCallable)
+	void JumpOffWall(bool isWallRunning);
+	
+	float wallJumpFactor = 750.0f;
+	float launchPower;
+	
+	FVector WallJumpingVector;
+	FVector CameraForwardVector;
 
 public:
 	// Called every frame
