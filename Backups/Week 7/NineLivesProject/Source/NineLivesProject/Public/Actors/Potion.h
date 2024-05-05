37bd348332dd5c42af315781091769f40fd21e41
Evin Oxley <91544713+EvinIkuyama@users.noreply.github.com>
2024-05-05 09:28:55 +0100

    Week 7

---
+++ Backups/Week 7/NineLivesProject/Source/NineLivesProject/Public/Actors/Potion.h
@@ -0,0 +1,46 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/SphereComponent.h"
+#include "GameFramework/Actor.h"
+#include "Interface/Pickup.h"
+#include "Potion.generated.h"
+
+UENUM(BlueprintType)
+enum class Enum_ActiveElement : uint8
+{
+	Fire,
+	Wind,
+	Water,
+	Earth,
+	Null,
+};
+
+UCLASS()
+class NINELIVESPROJECT_API APotion : public AActor, public IPickup
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	APotion();
+
+	UPROPERTY(BlueprintReadWrite,EditAnywhere)
+	TObjectPtr<USphereComponent> Collider;
+	
+	UPROPERTY(BlueprintReadWrite,EditAnywhere)
+	TObjectPtr<UStaticMeshComponent> Mesh;
+
+	UPROPERTY(BlueprintReadWrite,EditAnywhere)
+	Enum_ActiveElement Type;
+
+	UFUNCTION(BlueprintCallable)
+	virtual void PickedUp_Implementation(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool
+	                                     bFromSweep, const FHitResult& SweepResult);
+protected:
+	
+
+
+};
