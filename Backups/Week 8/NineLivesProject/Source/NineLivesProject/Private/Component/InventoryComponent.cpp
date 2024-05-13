991730ea9795f2f62fbe27fac71101e93013a929
Evin Oxley <91544713+EvinIkuyama@users.noreply.github.com>
2024-05-13 09:32:55 +0100

    Week 8

---
+++ Backups/Week 8/NineLivesProject/Source/NineLivesProject/Private/Component/InventoryComponent.cpp
@@ -0,0 +1,18 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+
+#include "Component/InventoryComponent.h"
+
+
+// Sets default values for this component's properties
+UInventoryComponent::UInventoryComponent()
+{
+	
+}
+
+void UInventoryComponent::AddToInventory(APotion* PotionToAdd)
+{
+	Inventory.Add(PotionToAdd);
+}
+
---
+++ Backups/Week 8/NineLivesProject/Source/NineLivesProject/Public/Component/InventoryComponent.h
@@ -0,0 +1,27 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Actors/Potion.h"
+#include "Components/ActorComponent.h"
+#include "InventoryComponent.generated.h"
+
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class NINELIVESPROJECT_API UInventoryComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this component's properties
+	UInventoryComponent();
+
+	UPROPERTY(EditAnywhere,BlueprintReadWrite)
+	TArray<TObjectPtr<APotion>> Inventory;
+	
+	UPROPERTY(EditAnywhere,BlueprintReadWrite)
+	int HeldIndex;
+
+	void AddToInventory(APotion* PotionToAdd);
+};
