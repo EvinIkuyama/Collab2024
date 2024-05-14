b064259c3cacebb301b16e89d12c8e95ecc623d5
LukeRogers30 <92024856+LukeRogers30@users.noreply.github.com>
2024-05-14 19:34:17 +0100

    level 3

---
+++ Main Project/NineLivesProject 5.2/Source/NineLivesProject/GameInstanceSave.cpp
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GameInstanceSave.h"
+
+void UGameInstanceSave::CallLevelData(int levelNumber, int& levelRetries, float& levelSeconds, int& levelStars)
+{
+	levelRetries = levelSaveRetries[levelNumber];
+	levelSeconds = levelSaveSeconds[levelNumber];
+	levelStars = levelSaveStars[levelNumber];
+}
+
+void UGameInstanceSave::SaveLevelData(int levelNumber, int levelRetries, float levelSeconds, int levelStars)
+{
+	levelSaveRetries[levelNumber] = levelRetries;
+	levelSaveSeconds[levelNumber] = levelSeconds;
+	levelSaveStars[levelNumber] = levelStars;
+}
---
+++ Main Project/NineLivesProject 5.2/Source/NineLivesProject/Private/Actors/Potion.cpp
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Actors/Potion.h"
+
+#include "Component/InventoryComponent.h"
+
+// Sets default values
+APotion::APotion()
+{
+	Collider = CreateDefaultSubobject<USphereComponent>(TEXT("Collider"));
+	SetRootComponent(Collider);
+	
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	Mesh->SetupAttachment(Collider);
+
+	Collider->OnComponentBeginOverlap.AddUniqueDynamic(this,&APotion::PickedUp_Implementation);
+}
+
+void APotion::PickedUp_Implementation(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult)
+{
+	TObjectPtr<UInventoryComponent> inv = nullptr;
+	if(OtherActor->GetOwner()->GetComponentByClass<UInventoryComponent>() != nullptr)
+	{
+		inv = OtherActor->GetOwner()->GetComponentByClass<UInventoryComponent>();
+	}
+	
+	if(inv != nullptr)
+	{
+		GEngine->AddOnScreenDebugMessage(1,5.0f,FColor::Red,TEXT("PICKEDUP"));
+		inv->AddToInventory(this);
+	}
+}
